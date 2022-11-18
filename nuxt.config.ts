76886029f115e0394a506fdea0a8909cc2e148b4
Benjamin VIELLARD <gbicou@gmail.com>
2022-11-18 10:34:56 +0100

    Initial commit

---
+++ nuxt.config.ts
@@ -0,0 +1,10 @@
+// https://v3.nuxtjs.org/api/configuration/nuxt.config
+export default defineNuxtConfig({
+    modules: [
+        '@nuxt/image-edge',
+    ],
+    image: {
+    //    provider: 'vercel'
+    //    dir: 'assets/images'
+    }
+})
---
+++ pages/index.vue
@@ -0,0 +1,8 @@
+<template>
+  <div>
+    <nuxt-picture src="/20220822133216_IMG_0023.JPG" sizes="sm:100vw md:50vw lg:400px" />
+  </div>
+</template>
+
+<script setup lang="ts">
+</script>
